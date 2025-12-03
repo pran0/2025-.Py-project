29f27c2e056c99347742c2eed859ca7810d35f01
Dave-dotc <obanladavid166@gmail.com>
2025-12-03 23:33:36 +0000

    Improve data handling and add game recommendation
    Refactor data cleaning process and add recommendation feature.

--- 2025-.Py-project/cleaned_up.py
+++ 2025-.Py-project/cleaned_up.py
@@ -1,66 +1,87 @@
-#Note: Any comment with ** at the end should be taken out before submission and even the line itself is not needed at the end of the code
-import pandas as pd
+#Note: Under construction when I am done I will update it again and i added that recommend feature cause i liked the idea 
+#But I don't really like the leaderboard thing because their aren't any players in this it's just game sales(my opinion)
+from pathlib import Path
+import csv
 
-df = pd.read_csv('video_games_sales.csv')
+path = Path('video_games_sales.csv')
+lines = path.read_text(encoding='utf-8').splitlines()
 
-cleaned_df = {
-    "Rank": df['rank'],
-    "Name": df['name'],
-    "Platform": df['platform'],
-    "Year": df['year'],
-    "Genre": df['genre'],
-    "Publisher": df['publisher'],
-    "NA_Sales": df['na_sales'],
-    "EU_Sales": df['eu_sales'],
-    "JP_Sales": df['jp_sales'],
-    "Global_Sales": df['global_sales']
-}
+reader = csv.reader(lines)
+header_row = next(reader)
 
-#The class is subject to change depending on how out code works beacuse the I don't think the init method is correct **
-# A class to clean to the data
-class CleanData:
-    #These are all the columns in the dataset below 
-    def __init__(self, rank, name, platform, year, genre, publisher, na_sales, eu_sales, jp_sales, other_sales, global_sales):
-        self.rank = rank
-        self.name = name
-        self.platform = platform
-        self.genre = genre
-        self.publisher = publisher
+Rank, Name, Platform, Year, Genre, Publisher, Na_sales, Eu_sales, Jp_sales, Other_sales, Global_sales = [], [], [], [], [], [], [], [], [], [], []
 
-        #The columns below are all integers/floats
-        self.year = year
-        self.na_sales = na_sales
-        self.eu_sales = eu_sales
-        self.jp_sales = jp_sales
-        self.other_sales = other_sales
-        self.global_sales = global_sales
+for row in reader:
+    rank = str(row[0])
+    name = str(row[1])
+    platform = str(row[2])
+    year = str(row[3])
+    genre = str(row[4])
+    publisher = str(row[5])
+    na_sales = str(row[6])
+    eu_sales = str(row[7])
+    jp_sales = str(row[8])
+    other_sales = str(row[9])
+    global_sales = str(row[10])
+    Rank.append(rank)
+    Name.append(name)
+    Platform.append(platform)
+    Year.append(year)
+    Genre.append(genre)
+    Publisher.append(publisher)
+    Na_sales.append(na_sales)
+    Eu_sales.append(eu_sales)
+    Jp_sales.append(jp_sales)
+    Other_sales.append(other_sales)
+    Global_sales.append(global_sales)
 
-    #This function cleans string values by removing whitespace
-    def clean_String(self, value):
-        if self.value == str:
-            self.value = value
-            self.value = df[f"{self.value}"].str.strip()
-            return self.value
+Rank_values = [int(x) for x in Rank]
+Name_values = [str(x) for x in Name]
+Platform_values = [str(x) for x in Platform]
+Year_values = [float(x) for x in Year]
+Genre_values = [str(x) for x in Genre]
+Publisher_values = [str(x) for x in Publisher]
+Na_sales_values = [float(x) for x in Na_sales]
+Eu_sales_values = [float(x) for x in Eu_sales]
+Jp_sales_values = [float(x) for x in Jp_sales]
+Other_sales_values = [float(x) for x in Other_sales]
+Global_sales_values = [float(x) for x in Global_sales]
+
+def recommend():
+    question = input("Do you want to enter a game name(Y)? or Do you want to some to be recommended to you(N)?:")
+    if question == "Y":
+        selected_name = input("Enter the name of the game: ")
+        #This converts the elements of Name into a string
+        selected_name = Name_values
+        if selected_name in Name:
+            print(f"{selected_name} The list below are top five most sold games "/
+                  f"{Name[0:4]}" )
         else:
-            return "Not a String"
-        
-    #This function converts any strings in the dataset to integers
-    def clean_value(self, numeric_value):
-
-        self.numeric_value = numeric_value
-
-        #Whoever can make this function select between int and float please do so because its driving me crazy and I can't be arsed to try it again **
-        if isinstance(self.numeric_value, float):
-            return self.numeric_value.astype(str).str.strip().astype(float)
-        else:
-            if isinstance(self.numeric_value, str):
-                self.numeric_value = df[f"{self.numeric_value}"].astype(str).str.strip().astype(float)
-                return self.numeric_value
-            elif isinstance(self.numeric_value, float):
-                return self.numeric_value
+            print("The name you entered is not a part of the games available")
+            ans = input("Would you like to see a few of the games listed (Y/N):")
+            if ans == "Y":
+                print(Name[0:50])
+            elif ans == "N":
+                print("Okay that's fine")
             else:
-                return self.numeric_value.astype(str).str.strip().astype(float)
-        
-cleanData = CleanData('Rank', 'Name', 'Platform', 'Year', 'Genre', 'Publisher', 'NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales', 'Global_Sales')
-cleaned_Int = cleanData.clean_value(cleaned_df['Year'])
-print(cleaned_Int)
+                print("")
+
+    elif question == "N":
+        #This selectes all apperances of the action genre in the dataset and converts it to an array that has the index
+        genre_type = "Action"
+        indices_g = [i for i, x in enumerate(Genre) if x == genre_type]
+
+        #This converts all the values into a float
+        indices_gs = Global_sales_values
+
+        #This for loop indexes all the elements of the "Global Sales" array
+        index_gs = []
+        for index, value in enumerate(indices_gs):
+            index_gs.append(index)
+        print(index_gs)
+
+        #To get the common values in a set
+        common = set(indices_g) & set(index_gs)
+        print(common)
+
+recommend()
